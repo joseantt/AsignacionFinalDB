0cd523a2521e7afba1f00ee923be4c7f704de16c
joseantt <89857273+joseantt@users.noreply.github.com>
2023-07-20 13:22:15 -0400

    GUI

---
+++ src/Visual/AgregarAsignatura.java
@@ -0,0 +1,116 @@
+package Visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JSpinner;
+
+public class AgregarAsignatura extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			AgregarAsignatura dialog = new AgregarAsignatura();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public AgregarAsignatura() {
+		setTitle("Agregar Asignatura");
+		setBounds(100, 100, 627, 379);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel lblNewLabel = new JLabel("C\u00F3digo:");
+			lblNewLabel.setBounds(36, 67, 56, 16);
+			contentPanel.add(lblNewLabel);
+		}
+		{
+			textField = new JTextField();
+			textField.setBounds(104, 64, 171, 22);
+			contentPanel.add(textField);
+			textField.setColumns(10);
+		}
+		{
+			JLabel lblNewLabel_1 = new JLabel("Nombre:");
+			lblNewLabel_1.setBounds(36, 134, 56, 16);
+			contentPanel.add(lblNewLabel_1);
+		}
+		{
+			textField_1 = new JTextField();
+			textField_1.setBounds(104, 131, 171, 22);
+			contentPanel.add(textField_1);
+			textField_1.setColumns(10);
+		}
+		{
+			JLabel lblCreditos = new JLabel("Creditos:");
+			lblCreditos.setBounds(36, 208, 56, 16);
+			contentPanel.add(lblCreditos);
+		}
+		{
+			JSpinner spinner = new JSpinner();
+			spinner.setBounds(113, 205, 56, 22);
+			contentPanel.add(spinner);
+		}
+		{
+			JLabel lblHorasTericas = new JLabel("Horas te\u00F3ricas:");
+			lblHorasTericas.setBounds(190, 208, 87, 16);
+			contentPanel.add(lblHorasTericas);
+		}
+		{
+			JSpinner spinner = new JSpinner();
+			spinner.setBounds(298, 205, 56, 22);
+			contentPanel.add(spinner);
+		}
+		{
+			JLabel lblHorasPrs = new JLabel("Horas pr\u00E1cticas:");
+			lblHorasPrs.setBounds(375, 208, 101, 16);
+			contentPanel.add(lblHorasPrs);
+		}
+		{
+			JSpinner spinner = new JSpinner();
+			spinner.setBounds(497, 205, 56, 22);
+			contentPanel.add(spinner);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setBorder(new LineBorder(Color.LIGHT_GRAY));
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("Agregar");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancelar");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
