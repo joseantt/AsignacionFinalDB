4b400a5feb19da93d3ba7b7c6978edb0c6841921
joseantt <89857273+joseantt@users.noreply.github.com>
2023-07-20 22:53:13 -0400

    Creación clase DB

---
+++ src/Database/ConexionDB.java
@@ -0,0 +1,27 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+	
+	public static Connection conectarDB() {
+	
+		String url = "jdbc:sqlserver://LAPTOP-KQA2PJRS;"
+				+ "Database=RegistroAcademico;"
+				+ "IntegratedSecurity=true;"
+				+ "TrustServerCertificate=True;";
+		
+		try {
+			try(Connection connection = DriverManager.getConnection(url)){
+				return connection;
+			}
+		} catch (SQLException e) {
+			System.out.println("Error al conectar la base de datos.");
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+}
