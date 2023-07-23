911144095079abceb150426d64139368c1949cd8
joseantt <89857273+joseantt@users.noreply.github.com>
2023-07-22 20:28:14 -0400

    Implementación del horario de estudiante

---
+++ src/Visual/BuscarEstudiante.java
@@ -0,0 +1,110 @@
+package Visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import Database.ConexionDB;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+
+public class BuscarEstudiante extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField txtMatricula;
+	private JTextField txtPeriodoAcad;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			BuscarEstudiante dialog = new BuscarEstudiante();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public BuscarEstudiante() {
+		setTitle("Horario estudiante");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Matr\u00EDcula:");
+		lblNewLabel.setBounds(41, 62, 112, 16);
+		contentPanel.add(lblNewLabel);
+		
+		JLabel lblPeriodoAcadmico = new JLabel("Periodo acad\u00E9mico:");
+		lblPeriodoAcadmico.setBounds(26, 140, 120, 16);
+		contentPanel.add(lblPeriodoAcadmico);
+		
+		txtMatricula = new JTextField();
+		txtMatricula.setBounds(121, 59, 205, 22);
+		contentPanel.add(txtMatricula);
+		txtMatricula.setColumns(10);
+		
+		txtPeriodoAcad = new JTextField();
+		txtPeriodoAcad.setColumns(10);
+		txtPeriodoAcad.setBounds(158, 137, 168, 22);
+		contentPanel.add(txtPeriodoAcad);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton btnBuscar = new JButton("Buscar");
+				btnBuscar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						String[] pkNombres = {"Matricula"};
+						String[] pkValores = {txtMatricula.getText()};
+						String[] pkNombres2 = {"CodPeriodoAcad"};
+						String[] pkValores2 = {txtPeriodoAcad.getText()};
+						
+						boolean estudianteExiste = false;
+						boolean periodoExiste = false;
+						try {
+							estudianteExiste = ConexionDB.buscarFilasTabla("Estudiante", pkNombres, pkValores, 1).next();
+							periodoExiste = ConexionDB.buscarFilasTabla("PeriodoAcademico", pkNombres2, pkValores2, 1).next();
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+						if(estudianteExiste && periodoExiste) {
+							HorarioEstudiante horarioEst = new HorarioEstudiante(
+															txtMatricula.getText(), txtPeriodoAcad.getText());
+							horarioEst.setVisible(true);
+							dispose();
+						}else {
+							JOptionPane.showMessageDialog(null,"Uno de los campos no es valido","Error",JOptionPane.INFORMATION_MESSAGE);
+						}
+					}
+				});
+				btnBuscar.setActionCommand("OK");
+				buttonPane.add(btnBuscar);
+				getRootPane().setDefaultButton(btnBuscar);
+			}
+			{
+				JButton btnCancelar = new JButton("Cancelar");
+				btnCancelar.setActionCommand("Cancel");
+				buttonPane.add(btnCancelar);
+			}
+		}
+	}
+}
---
+++ src/Visual/HorarioEstudiante.java
@@ -0,0 +1,123 @@
+package Visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
+
+import Database.ConexionDB;
+import javafx.scene.shape.Box;
+
+import java.awt.Color;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+
+public class HorarioEstudiante extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTable tablaHorario;
+
+	/**
+	 * Launch the application.
+	 */
+	/*public static void main(String[] args) {
+		try {
+			HorarioEstudiante dialog = new HorarioEstudiante("10147015", "320222023");
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}*/
+
+	/**
+	 * Create the dialog.
+	 */
+	public HorarioEstudiante(String matricula, String codPeriodoAcad) {
+		setTitle("HorarioEstudiante");
+		setBounds(100, 100, 976, 580);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JPanel panel = new JPanel();
+			panel.setBorder(new LineBorder(Color.LIGHT_GRAY));
+			contentPanel.add(panel, BorderLayout.CENTER);
+			panel.setLayout(new BorderLayout(0, 0));
+			{
+				JScrollPane scrollPane = new JScrollPane();
+				panel.add(scrollPane, BorderLayout.CENTER);
+				{
+					tablaHorario = new JTable();
+					tablaHorario.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+					DefaultTableModel model = (DefaultTableModel) tablaHorario.getModel();
+					String[] columnas = {"", "", "", "", "", ""};
+					model.setColumnIdentifiers(columnas);
+					actualizarFilasHorario(model, matricula, codPeriodoAcad);
+					scrollPane.setViewportView(tablaHorario);
+				}
+			}
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setBorder(new LineBorder(Color.LIGHT_GRAY));
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton btnCancelar = new JButton("Cancelar");
+				btnCancelar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						dispose();
+					}
+				});
+				btnCancelar.setActionCommand("Cancel");
+				buttonPane.add(btnCancelar);
+			}
+		}
+	}
+
+	private void actualizarFilasHorario(DefaultTableModel model, String matricula, String codPeriodoAcad) {
+		Connection conexion = ConexionDB.conectarDB();
+		model.setRowCount(0);
+		try {
+			String sql = "EXEC dbo.HorarioEstudiante '"+matricula+"', '"+codPeriodoAcad+"'";
+			System.out.println(sql);
+			PreparedStatement p = conexion.prepareStatement(sql);
+			ResultSet rs = p.executeQuery();
+			
+			while (rs.next()) {
+	            String c1 = rs.getString("C1");
+	            String c2 = rs.getString("C2");
+	            String c3 = rs.getString("C3");
+	            String c4 = rs.getString("C4");
+	            String c5 = rs.getString("C5");
+	            String c6 = rs.getString("C6");
+	            String[] fila = {c1, c2, c3, c4, c5, c6};
+	            model.addRow(fila);
+	        }
+			p.close();
+			conexion.close();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		} 
+	}
+
+}
